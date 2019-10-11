a9715d59edb5f067e6186ee36f37c28380d29b62
Jazmin M <jazminmerckel@gmail.com>
2019-10-11 23:55:47 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -34,8 +34,11 @@ class Book
   def turn_page
   end
   
-  
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
   
 end
 
 Book.new("And Then There Were None")
+book.turn_page
