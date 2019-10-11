596506309c4a4c6f5b42923e8ed1cfe51da5d001
Jazmin M <jazminmerckel@gmail.com>
2019-10-11 23:54:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,26 @@
 class Book
+  
   def initialize(title) #corresponds with .new 
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def author=(author)
+    @author = author
+    
+  def author
+    @author
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
+  def page_count
+    @page_count
   end
 end
 
